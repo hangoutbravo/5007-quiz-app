3fbb00673fde2a6c61a725119e5c93e862b0dde5
hangoutbravo <hangoutbravo@qq.com>
2026-07-15 21:39:42 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
 
-const CACHE='q5007-v1',FILES=['./','./index.html','./style.css','./app.js','./questions.js','./manifest.webmanifest','./icon.svg'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='q5007-v2',FILES=['./','./index.html','./style.css','./app.js','./questions.js','./questions2.js','./manifest.webmanifest','./icon.svg'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
